8160033b8e1ef85360c29654e967caf705469a5e
KateSTento <bktento@skymesh.com.au>
2016-06-16 05:47:03 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grammable::Application.config.secret_key_base = '14746708d8fc3aae534c2380311c177b0bd93b3ecfab336f1199adf5ecde6bda06c906dc58610a127f5ab8db4f7baf1c3d6cdaa6863117245dd851042913d548'
